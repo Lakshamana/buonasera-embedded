7185ee0ff403b5fe694e85e785f2a6f11ec7fa11
Lakhshamana <lakshamana@protonmail.com>
2025-11-20 12:38:10 -0300

    chore: use registers instead

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate cortex_m_rt;
 extern crate cortex_m_semihosting;
 
 use core::panic::PanicInfo;
-use cortex_m::{asm, peripheral::syst::SystClkSource, Peripherals};
+use cortex_m::asm;
 use cortex_m_rt::{entry, exception};
 use cortex_m_semihosting::hprintln;
 
@@ -15,6 +15,15 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// System tick - control and status register
+const SYST_CSR: u32 = 0xE000E010;
+
+// System tick - reload value register
+const SYST_RVR: u32 = 0xE000E014;
+
+// System tick - current value register
+const SYST_CVR: u32 = 0xE000E018;
+
 // CPU Frequency (12.5MHz)
 const CPU_FREQ: u32 = 12_500_000;
 
@@ -22,14 +31,18 @@ const CPU_FREQ: u32 = 12_500_000;
 fn main() -> ! {
     hprintln!("Hello World!");
 
-    let peripherals = Peripherals::take().unwrap();
+    let sleep_time: u32 = CPU_FREQ; // 1s
 
-    let mut systick = peripherals.SYST;
-    systick.enable_interrupt();
-    systick.set_clock_source(SystClkSource::Core);
-    systick.set_reload(CPU_FREQ);
-    systick.clear_current();
-    systick.enable_counter();
+    unsafe {
+        // set timer value (reload value register)
+        *(SYST_RVR as *mut u32) = sleep_time;
+
+        // Clear current value register
+        *(SYST_CVR as *mut u32) = 0;
+
+        // enable timer
+        *(SYST_CSR as *mut u32) = 0b111;
+    }
 
     loop {
         asm::wfi();
